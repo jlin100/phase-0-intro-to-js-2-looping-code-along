7727f9e98736897627aa2bdf1d687b4a1aaa20b0
Jeff Lin <thejefflin@gmail.com>
2022-04-11 17:51:33 -0400

    commit tree d193e1768b05a5545abb28d36b9fb6a9850cbdc0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, name) {
+    let messages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        messages.push((`Thank you, ${namesArray[i]}, for the wonderful ${name} gift!`));
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number > -1) {
+        let decrease = number --;
+        console.log(decrease);
+    }
+
+}
